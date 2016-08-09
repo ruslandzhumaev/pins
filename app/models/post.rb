cbbee4da8ba9486426759e185a15df87199be887
ruslandzhumaev <ruslandzhumeav@gmail.com>
2016-08-08 22:26:39 -0700

    Add image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  has_attached_file :image, styles: { medium: "600x600>", thumb: "200x200>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   belongs_to :user
 end
