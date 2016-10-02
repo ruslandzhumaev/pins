9cb05b737d8935de0d147287be17cde4046b375a
ruslandzhumaev <ruslandzhumeav@gmail.com>
2016-10-02 13:53:47 +0300

    Categories fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
 
-  resources :posts do
-  	get ":category_id", action: :index, on: :collection
-  end
+  resources :posts
 end
